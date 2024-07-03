9bdd740b7853bce94b716bb1705c4cb8524e5c36
Sithu <cithukyaw@gmail.com>
2024-07-03 23:25:39 +0630

    Init repo with counter component

---
+++ src/components/Counter.js
@@ -0,0 +1,10 @@
+const Counter = () => {
+  return (
+      <div>
+          <button className="success">Increment</button>
+          <button className="danger">Decrement</button>
+      </div>
+  );
+};
+
+export default Counter;
