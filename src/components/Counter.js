c6e6808a4b0573ef89e06bb74bf67003efc896ab
Sithu <cithukyaw@gmail.com>
2024-07-04 23:26:25 +0630

    Implement actions and reducers for increment/decrement

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,26 @@
+import {useDispatch, useSelector} from "react-redux";
+import {decrement, increment, incrementByAmount, decrementByAmount, reset} from "../state/counter/counterSlice";
+
 const Counter = () => {
-  return (
-      <div>
-          <button className="success">Increment</button>
-          <button className="danger">Decrement</button>
-      </div>
-  );
+    const count = useSelector(state => state.counter.value);
+    const dispatch = useDispatch();
+
+    return (
+        <div>
+            <h1>{count}</h1>
+            <p>
+                <button className="success" onClick={() => dispatch(increment())}>Increment</button>
+                <button className="danger" onClick={() => dispatch(decrement())}>Decrement</button>
+            </p>
+            <p>
+                <button className="success" onClick={() => dispatch(incrementByAmount(10))}>Increment By 10</button>
+                <button className="danger" onClick={() => dispatch(decrementByAmount(10))}>Decrement By 10</button>
+            </p>
+            <p>
+                <button onClick={() => dispatch(reset())}>Reset</button>
+            </p>
+        </div>
+    );
 };
 
 export default Counter;
